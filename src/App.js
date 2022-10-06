58fe72ab663daf4557474a59a0a17935ff3c708b
AlliSuresh-ZLR <suresh.a@zelarsoft.com>
2022-10-07 00:00:20 +0530

    some unwanted imported files removed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Home from './Home';
-import Profile from './Profile';
-
-
 function App() {
   return (
     <div className="App">
       <center>
         <h1>hello
-          <Home/>
-          <Profile/>
+         
         </h1>
       </center>
     </div>
